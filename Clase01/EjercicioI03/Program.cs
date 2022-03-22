ac541967ae66a59db18c6bf681d0a0d7c87e7287
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-03-22 19:07:40 -0300

    ejercicios hasta el 04

---
+++ Clase01/EjercicioI03/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace EjercicioI03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroInt;
+            bool continuar = false;
+            string userInput;
+
+            Console.WriteLine("Desea comenzar? S (si) o N (no)");
+            userInput = Console.ReadLine();
+
+            while (userInput != "S" && userInput != "s" && userInput != "N" && userInput != "n")
+            {
+                Console.WriteLine("Favor ingresar una de las opciones. S (si) o N (no)");
+                userInput = Console.ReadLine();
+            }
+            if (userInput == "S" || userInput == "s")
+            {
+                continuar = true;
+            }
+            else
+            {
+                Console.WriteLine("Cerrando sistema...");
+            }
+
+            while (continuar)
+            {
+                Console.WriteLine("Ingrese un numero");
+                userInput = Console.ReadLine();
+
+                while (!int.TryParse(userInput, out numeroInt))
+                {
+                    Console.WriteLine("Error, ingrese un numero valido o presione N para salir");
+                    userInput = Console.ReadLine();
+                    if (userInput == "S" || userInput == "s")
+                    {
+                        continuar = true;
+                    }
+                    else if (userInput == "N" || userInput == "n")
+                    {
+                        continuar = false;
+                        Console.WriteLine("Cerrando sistema...");
+                        break;
+                    }
+                }
+
+                for (int i = numeroInt; i > 0; i--)
+                {
+                    int divisores = 0;
+                    int j = 1;
+
+                    while (j <= i)
+                    {
+                        if (i % j == 0)
+                        {
+                            divisores++;
+                        }
+
+                        j++;
+                    }
+
+                    if (divisores <= 2)
+                    {
+                        Console.WriteLine("El numero {0} es primo", i);
+                    }
+                }
+
+                Console.WriteLine("Desea continuar? S (si) o N (no)");
+                userInput = Console.ReadLine();
+
+                while (userInput != "S" && userInput != "s" && userInput != "N" && userInput != "n")
+                {
+                    Console.WriteLine("Favor ingresar una de las opciones. S (si) o N (no)");
+                    userInput = Console.ReadLine();
+                }
+                if (userInput == "S" || userInput == "s")
+                {
+                    continuar = true;
+                }
+                else if (userInput == "N" || userInput == "n")
+                {
+                    continuar = false;
+                    Console.WriteLine("Cerrando sistema...");
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Clase01/EjercicioI04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EjercicioI04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contadorPerfectos = 0;
+            for (int i = 1; contadorPerfectos < 5; i++)
+            {
+                int acumulador = 0;
+
+                int j = 1;
+
+                while (j < i)
+                {
+                    if (i % j == 0)
+                    {
+                        acumulador = acumulador + j;
+                    }
+                    j++;
+                }
+
+                if (acumulador == i)
+                {
+                    Console.WriteLine("El numero {0} es perfecto", i);
+                    contadorPerfectos++;
+                }
+            }
+        }
+    }
+}
