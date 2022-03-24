3b0edec8e94f22c8dd7a86e3835a1049a334ece5
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-03-24 16:20:32 -0300

    ejercicios 6 unidad 1 hecho

---
+++ Clase01/EjercicioI06/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace EjercicioI06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int inicio;
+            int fin;
+
+            Console.WriteLine("Ingrese un año");
+            userInput = Console.ReadLine();
+
+            while(!int.TryParse(userInput,out inicio))
+            {
+                Console.WriteLine("Error, ingrese un año valido");
+                userInput = Console.ReadLine();
+            }
+
+            Console.WriteLine("Ingrese un año");
+            userInput = Console.ReadLine();
+
+            while (!int.TryParse(userInput, out fin))
+            {
+                Console.WriteLine("Error, ingrese un año valido");
+                userInput = Console.ReadLine();
+            }
+
+            for (int i = inicio; i < fin; i++)
+            {
+                if(i%4==0)
+                {
+                    if (i % 100 == 0)
+                    {
+                        if (i % 400 == 0)
+                        {
+                            Console.WriteLine("El año {0} es bisiesto",i);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("El año {0} es bisiesto",i);
+                    }
+                }
+            }
+        }
+    }
+}
