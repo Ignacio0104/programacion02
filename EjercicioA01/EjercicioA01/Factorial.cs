1157dd5945c085bd349feab8fbd04b4809d4a753
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-03-26 15:23:57 -0300

    unidad 2 terminada

---
+++ EjercicioA01/EjercicioA01/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioA01
+{
+    internal class Factorial
+    {
+        public static double CalcularFactorial(int numero)
+        {
+            double result = 1;
+
+            for (int i = 1; i < numero; i++)
+            {
+                result *= i + 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ EjercicioA01/EjercicioA01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EjercicioA01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial.CalcularFactorial(9));
+        }
+    }
+}
