89a20c08902705dd6fdf9a9eff97bb0df6ee4783
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-03-26 11:47:53 -0300

    ejercicio 05 unidad 02

---
+++ EjercicioI05-Unidad02/EjercicioI05-Unidad02/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace EjercicioI05_Unidad02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int numeroIngresado;
+            bool continuar = false;
+
+            Console.WriteLine("Desea comenzar? S o N");
+            userInput = Console.ReadLine();
+
+            while(userInput.ToLower()!="s"&& userInput.ToLower() != "n")
+            {
+                Console.WriteLine("Ingrese una opcion valida: S o N");
+                userInput = Console.ReadLine();
+            }
+
+            if (userInput.ToLower() == "s")
+            {
+                continuar = true;
+            }
+            else
+            {
+                continuar = false;
+                Console.WriteLine("Cerrando el sistema...");
+            }
+
+
+            while (continuar)
+            {
+                Console.WriteLine("Ingrese un numero");
+                userInput = Console.ReadLine();
+                while (!int.TryParse(userInput, out numeroIngresado))
+                {
+                    Console.WriteLine("Error, ingrese un numero valido");
+                    userInput = Console.ReadLine();
+                }
+
+                Console.WriteLine(Tablas.Calcular(numeroIngresado));
+
+                Console.WriteLine("Desea continuar? S o N");
+                userInput = Console.ReadLine();
+
+                while (userInput.ToLower() != "s" && userInput.ToLower() != "n")
+                {
+                    Console.WriteLine("Ingrese una opcion valida: S o N");
+                    userInput = Console.ReadLine();
+                }
+
+                if (userInput.ToLower() == "s")
+                {
+                    continuar = true;
+                    Console.Clear();
+                }
+                else
+                {
+                    continuar = false;
+                    Console.WriteLine("Cerrando el sistema...");
+                }
+            }
+
+        }
+    }
+}
---
+++ EjercicioI05-Unidad02/EjercicioI05-Unidad02/Tablas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioI05_Unidad02
+{
+    internal class Tablas
+    {
+        public static StringBuilder Calcular(int numero)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            for(int i=1; i<= 10; i++)
+            {
+                int cuenta = numero * i;
+
+                resultado.AppendLine($"{numero} x {i} = {cuenta}");
+            }
+
+            return resultado;
+        }
+    }
+}
