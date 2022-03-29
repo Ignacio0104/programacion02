7453aa1f1ebe0579df2bc0e89bb455bd96008f13
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-03-29 16:48:03 -0300

    ejercicio 3 Unidad 3

---
+++ EjercicioI03-Unidad03/BibliotecaDeClases/Estudiante.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace BibliotecaDeClases
+{
+    public class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random rand;
+
+        static Estudiante()
+        {
+            rand = new Random();
+        }
+
+        public Estudiante(string nombre, string apellido, string legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }
+
+        public void SetNotaPrimerParcial(int nota)
+        {
+            this.notaPrimerParcial = nota;
+        }
+
+        public void SetNotaSegundoParcial(int nota)
+        {
+            this.notaSegundoParcial = nota;
+        }
+
+        private double CalcularPromedio()
+        {
+            double resultado = ((double)notaPrimerParcial + notaSegundoParcial) / 2;
+            return resultado;
+        }
+
+        public double CalcularNotaFinal()
+        {
+            double resultado = -1;
+            if (notaPrimerParcial >= 4 && notaSegundoParcial >= 4)
+            {
+                resultado = rand.Next(6,11);
+            }
+
+            return resultado;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Alumno: {nombre} {apellido} Legajo {legajo}");
+            sb.AppendLine($"Primer Parcial: {notaPrimerParcial} Segundo Parcial {notaSegundoParcial}");
+            sb.AppendLine($"Promedio: {CalcularPromedio()}");
+            if (CalcularNotaFinal() > 1)
+            {
+                sb.AppendLine($"Nota Final: {CalcularNotaFinal()}");
+            }
+            else
+            {
+                sb.AppendLine($"Nota Final: Desaprobado");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
--- EjercicioI03-Unidad03/EjercicioI03-Unidad03/Program.cs
+++ EjercicioI03-Unidad03/EjercicioI03-Unidad03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BibliotecaDeClases;
 
 namespace EjercicioI03_Unidad03
 {
@@ -6,7 +7,22 @@ namespace EjercicioI03_Unidad03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Estudiante estudiante1 = new Estudiante("Matias", "Perez", "10001");
+            Estudiante estudiante2 = new Estudiante("Ignacio", "Martin", "10002");
+            Estudiante estudiante3 = new Estudiante("Pedro", "Perez", "10003");
+
+            estudiante1.SetNotaPrimerParcial(10);
+            estudiante1.SetNotaSegundoParcial(6);
+
+            estudiante2.SetNotaPrimerParcial(8);
+            estudiante2.SetNotaSegundoParcial(6);
+
+            estudiante3.SetNotaPrimerParcial(3);
+            estudiante3.SetNotaSegundoParcial(3);
+
+            Console.WriteLine(estudiante1.Mostrar());
+            Console.WriteLine(estudiante2.Mostrar());
+            Console.WriteLine(estudiante3.Mostrar());
         }
     }
 }
