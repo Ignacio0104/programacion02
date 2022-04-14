51a9f0ee7810c4fdc38166ec0428adcf5322ed3a
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-04-14 19:41:25 -0300

    ejercicios contador de palabras

---
+++ COLECCIONES/EjercicioI03-Unidad06ContarPalabras/EjercicioI03-Unidad06ContarPalabras/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace EjercicioI03_Unidad06ContarPalabras
+{
+    partial class frmContador
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTxt = new System.Windows.Forms.RichTextBox();
+            this.btn_Calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTxt
+            // 
+            this.richTxt.Location = new System.Drawing.Point(12, 21);
+            this.richTxt.Name = "richTxt";
+            this.richTxt.Size = new System.Drawing.Size(610, 263);
+            this.richTxt.TabIndex = 0;
+            this.richTxt.Text = "";
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.Location = new System.Drawing.Point(460, 302);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(138, 33);
+            this.btn_Calcular.TabIndex = 1;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = true;
+            this.btn_Calcular.Click += new System.EventHandler(this.btn_Calcular_Click);
+            // 
+            // frmContador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(634, 360);
+            this.Controls.Add(this.btn_Calcular);
+            this.Controls.Add(this.richTxt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmContador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contador de Palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTxt;
+        private System.Windows.Forms.Button btn_Calcular;
+    }
+}
---
+++ COLECCIONES/EjercicioI03-Unidad06ContarPalabras/EjercicioI03-Unidad06ContarPalabras/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioI03_Unidad06ContarPalabras
+{
+    public partial class frmContador : Form
+    {
+        Dictionary<string, int> lista;
+        public frmContador()
+        {
+            InitializeComponent();
+            lista = new Dictionary<string, int>();
+        }
+
+        public void recorrerRichText()
+        {
+            string input = richTxt.Text;
+            StringBuilder palabra = new StringBuilder();
+
+            for (int i = 0; i < input.Length;  i++)
+            {
+                if(input[i].ToString()!= " ")
+                {
+                    palabra.Append(input[i]);
+                }
+                else
+                {
+                    if (lista.ContainsKey(palabra.ToString()))
+                    {
+                        lista[palabra.ToString()]++;
+                    }
+                    else
+                    {
+                        lista.Add(palabra.ToString(), 1);
+                    }
+                    palabra.Clear();
+                }
+            }
+        }
+
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+            recorrerRichText();
+            StringBuilder mensaje = new StringBuilder();
+            var sortedItem = from data in lista orderby data.Value descending select data;
+            int index = 1;
+            foreach (var item in sortedItem)
+            {
+                mensaje.AppendLine($"Puesto {index}: {item.Key} - {item.Value} veces");
+                if(index == 3)
+                {
+                    break;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+            MessageBox.Show(mensaje.ToString());
+        }
+    }
+
+}
