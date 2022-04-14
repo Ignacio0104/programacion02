e6832090e0119e1d6e06f326487c5cde8c8c5cc8
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-04-13 23:44:53 -0300

    ejercicios numeros locos

---
+++ COLECCIONES/EjercicioI01-Unidad06NumerosLocos/EjercicioI01-Unidad06NumerosLocos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EjercicioI01_Unidad06NumerosLocos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>();
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                Random rnd = new Random();
+                lista.Add(rnd.Next(-1000, 1000));
+            }
+
+            Mostrar(lista);
+            lista.Sort();
+            Console.WriteLine("Lista ordenada ascendente ---------------------------------");
+            Mostrar(lista);
+            lista.Sort(Criterio);
+            Console.WriteLine("Lista ordenada descendente ---------------------------------");
+            Mostrar(lista);
+
+
+        }
+
+        public static int Criterio(int elemento1, int elemento2)
+        {
+            return elemento2 - elemento1;
+        }
+
+
+        public static void Mostrar(List<int> coleccion)
+        {
+            foreach (var item in coleccion)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+
+    }
+}
