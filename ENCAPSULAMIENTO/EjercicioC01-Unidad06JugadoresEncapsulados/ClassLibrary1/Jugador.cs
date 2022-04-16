c88b8a13ff97624b089f87835b530713778c55c0
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-04-16 10:16:07 -0300

    correcion form_closing

---
+++ ENCAPSULAMIENTO/EjercicioC01-Unidad06JugadoresEncapsulados/ClassLibrary1/Jugador.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Jugador
+    {
+        private int dni;
+        private string nombre;
+        private int partidosJugados;
+        private int cantidadGoles;
+
+        private Jugador()
+        {
+            partidosJugados = 0;
+            cantidadGoles = 0;
+        }
+
+        public Jugador (int dni, string nombre):this()
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+        }
+
+        public Jugador (int dni,string nombre, int totalGoles, int totalPartidos) : this(dni, nombre)
+        {
+            this.cantidadGoles = totalGoles;
+            this.partidosJugados = totalPartidos;
+            
+        }
+
+        public int PartidosJugados
+        {
+            get
+            {
+                return this.partidosJugados;
+            }
+        }
+
+        public float PromedioGoles
+        {
+            get
+            {
+                return (float)this.cantidadGoles / this.partidosJugados;
+            }
+        }
+
+        public int TotalGoles
+        {
+            get
+            {
+                return this.cantidadGoles;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+
+            set
+            {
+                this.nombre = value;
+            }
+        }
+
+        public int Dni
+        {
+            get
+            {
+                return this.dni;
+            }
+
+            set
+            {
+                this.dni = value;
+            }
+        }
+
+
+        public static bool operator ==(Jugador j1, Jugador j2)
+        {
+            //Falta validar NULL
+            return j1.dni == j2.dni;
+        }
+
+        public static bool operator !=(Jugador j1, Jugador j2)
+        {
+            return !(j1 == j2);
+        }
+
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Nombre:{nombre}");
+            sb.AppendLine($"DNI:{dni}");
+            sb.AppendLine($"Cantidad Goles:{cantidadGoles}");
+            sb.AppendLine($"Partidos Jugados:{partidosJugados}");
+            sb.AppendLine($"Promedio de goles:{PromedioGoles}");
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ ENCAPSULAMIENTO/EjercicioC01-Unidad06JugadoresEncapsulados/EjercicioC01-Unidad06EstadisticaDeportiva/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary1;
+
+namespace EjercicioC01_Unidad06EstadisticaDeportiva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Equipo team = new Equipo(10, "Bulls");
+            Jugador j1 = new Jugador(1, "Pedro",20,10);
+            Jugador j2 = new Jugador(2, "Juan",5,10);
+            Jugador j3 = new Jugador(3, "Santiago",10,10);
+            Jugador j4 = new Jugador(4, "Tomas",3,10);
+            Jugador j5 = new Jugador(5, "Matias",5,10);
+            Jugador j6 = new Jugador(6, "Valentin",35,10);
+            Jugador j7 = new Jugador(7, "Sandro",2,10);
+            Jugador j8 = new Jugador(8, "Mauricio",5,10);
+            Jugador j9 = new Jugador(9, "Lucas",6,10);
+            Jugador j10 = new Jugador(10, "Sebastian",19,10);
+
+            Console.WriteLine(team + j1);
+            Console.WriteLine(team + j2);
+            Console.WriteLine(team + j3);
+            Console.WriteLine(team + j3);
+            Console.WriteLine(team + j5);
+            Console.WriteLine(team + j6);
+            Console.WriteLine(team + j7);
+            Console.WriteLine(team + j8);
+            Console.WriteLine(team + j9);
+            Console.WriteLine(team + j10);
+            Console.WriteLine(team + j10);
+
+            Console.WriteLine(team.jugadores.Count);
+
+            foreach (var item in team.jugadores)
+            {
+                Console.WriteLine(item.MostrarDatos());
+            }
+
+            
+        }
+    }
+}
